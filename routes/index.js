98a46bf712e56188ef1253df0557128da6958484
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-03-28 12:48:59 +0100

    login layout & login view

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 //@desc Login/Landing Page
 //@route GET /
 
-router.get("/", (req, res) => {
-  res.render("login");
+router.get('/', (req, res) => {
+  res.render('login', {
+    layout: 'login',
+  });
 });
 
 //@desc Dashboard
 //@route GET /dashboard
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
 });
 
 module.exports = router;
