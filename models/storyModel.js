642a9f98849153961b05f840c1b97b9058f40a4b
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-03-31 09:41:22 +0100

    story model created

---
+++ models/storyModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const storySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: 'public',
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Story = mongoose.model('Story', storySchema);
+
+module.exports = Story;
