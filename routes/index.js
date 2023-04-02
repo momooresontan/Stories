08217448e7ff1d828e4f647fa6b0b1a854689783
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-04-02 12:35:15 +0100

    dashboard creation

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const auth = require('../middlewares/authMiddleware');
+const Story = require('../models/storyModel');
 
 const router = express.Router();
 
@@ -15,8 +16,14 @@ router.get('/', auth.ensureGuest, (req, res) => {
 //@desc Dashboard
 //@route GET /dashboard
 
-router.get('/dashboard', auth.ensureAuth, (req, res) => {
-  res.render('dashboard', { name: req.user.firstName });
+router.get('/dashboard', auth.ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render('dashboard', { name: req.user.firstName, stories });
+  } catch (err) {
+    console.error(err);
+    res.render('error/500');
+  }
 });
 
 module.exports = router;
