cea2362491ed148c1f984f055f2a179a3566bd7b
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-04-22 09:20:45 +0100

    single user stories

--- routes/storiesRoute.js
+++ routes/storiesRoute.js
@@ -130,4 +130,25 @@ router.delete('/:id', auth.ensureAuth, async (req, res) => {
   }
 });
 
+//@desc User stories
+//@route GET /stories/user/:userId
+
+router.get('/user/:userId', auth.ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public',
+    })
+      .populate('user')
+      .lean();
+
+    res.render('stories/index', {
+      stories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render('error/500');
+  }
+});
+
 module.exports = router;
